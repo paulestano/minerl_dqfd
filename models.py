aaba27cc065eb3953d98e6379adc78e55166c04b
paulesta55 <paul.estano@ensea.fr>
2020-11-23 13:37:46 +0100

    add hidden layers

--- models.py
+++ models.py
@@ -67,4 +67,4 @@ class DQN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        return self.head(F.relu(self.hidden(x.view(x.size(0), -1))))
